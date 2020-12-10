e51c551dead47580af97c326c8bfd71267b810aa
ccalvocm <ccalvocm@gmail.com>
2020-12-10 16:46:16 -0300

    Update Hackaton_CCC.py

--- Scripts/Hackaton_CCC.py
+++ Scripts/Hackaton_CCC.py
@@ -34,7 +34,10 @@ def main(folder = '/home/carlos/Downloads', nc_cloud = 'ls8_toa_2013_2020_icesno
     
     nirswir = nirswir.assign_coords(time = times)
     nieve = nieve.assign_coords(time = times)  
-    
+    clip = xarray.DataArray(np.zeros((170,1679,1416)), coords=nirswir.coords)
+    clip = clip.assign_coords(time = times)  
+
+
     #coordenadas
     nirswir.rio.set_spatial_dims(x_dim="x", y_dim="y", inplace=True)
     nirswir.rio.write_crs("epsg:32719", inplace=True)
@@ -56,10 +59,7 @@ def main(folder = '/home/carlos/Downloads', nc_cloud = 'ls8_toa_2013_2020_icesno
     
         nc_ua  = nirswir[i,:,:]
         snow = nieve[i,:,:]
-            
-#        result = gaussian_filter(nc_ua, sigma=2)
-#        nc_ua.values = result  
-        
+                   
         thres = dicc[times[i].month]
         thres = 3
         
@@ -78,16 +78,24 @@ def main(folder = '/home/carlos/Downloads', nc_cloud = 'ls8_toa_2013_2020_icesno
         nc_ua = nc_ua.where(((nc_ua >= thres)), other=np.nan) 
 
         
-    ############# Calculate mask cuenca
+    # Calculate mask cuenca
     
         clipped = nc_ua.rio.clip(cobertura_nival.geometry.apply(mapping), cobertura_nival.crs, drop=False)
     
+        time = np.arange('2000', '2005', dtype='datetime64[D]')
+        test_array = xarray.DataArray(np.zeros(len(time)), coords={'time': time}, dims=['time'])    
+        ind_start = (test_array.indexes['time'] == pd.Timestamp('2001-01-01')).argmax()
+        ind_end = (test_array.indexes['time'] == pd.Timestamp('2001-12-31')).argmax()
+        test_array[ind_start:ind_end + 1] = np.ones(365)
     #numero pixeles glaciar dentro de la cuenca
+
+        clip[i] =   clipped.values   
         area_glaciar = np.count_nonzero(~np.isnan(clipped))*900/1e6
         areas_glaciares[i] = area_glaciar
         gc.collect()
         del nc_ua
         del snow
+        del clipped
   #%%          
     plt.close("all")
     plot_areas = pd.DataFrame(areas_glaciares, index = times)
@@ -99,5 +107,8 @@ def main(folder = '/home/carlos/Downloads', nc_cloud = 'ls8_toa_2013_2020_icesno
     ax.set_ylim(bottom =0)
     ax.grid()
     
+    clip.resample(time="1Y").mean()
+    media_first = clip.sel(time=slice('2013-01-01', '2015-12-31')).groupby('time.year').mean()
+
 if __name__ == '__main__':
     main()
